33b48c48b6b55b58a07b1c829d4a5794ab85f96c
Rick Tesmond <tesmonrd@gmail.com>
2016-04-12 20:23:14 -0700

    messin around

---
+++ imagersite/imagersite/views.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+# from django.http import HttpResponse
+# from django.shortcuts import render, render_to_response
+# from django.template import loader
+from django.views.generic import TemplateView
+
+
+# def home_page(request, *args, **kwargs):
+#     foo = 'garbanzo beans'
+#     return render(request, 'home.html', context={'foo': foo})
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self):
+        foo = 'garbanzo beans'
+        return {'foo': foo}
