6784081f8ca8c5f182bddbba37db8c2d43997efb
Mike <mikekh@uw.edu>
2016-04-27 16:42:25 -0700

    Added imager API and modififed requirements.txt

---
+++ imagersite/imager_api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .imager_images.models import Photo
+
+
+class PhotoSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Photo
+        fields = ['user', 'image_file', 'image_title', 'image_description', 
+                  'date_uploaded', 'date_modified', 'date_published', 'published']
