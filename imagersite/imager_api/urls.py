8300c0fce0e9699b96f7351462b04f2b696b046d
Rick Tesmond <tesmonrd@gmail.com>
2016-04-30 23:54:51 -0700

    removed redundant url

--- imagersite/imager_api/urls.py
+++ imagersite/imager_api/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url, include
 from api import views
-from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
 
 
@@ -13,5 +12,3 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework'))
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
